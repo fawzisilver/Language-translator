3014c7b3ee9fd6ad705e7087d265a4c77ee6f239
fawzisilver <90504301+fawzisilver@users.noreply.github.com>
2024-11-08 14:01:43 -0700

    initial design of translator app

---
+++ components/translator.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Translator = () => {
+	return (
+		<main className=" flex flex-col justify-center items-center p-12">
+			<div className="flex flex-col justify-center w-full h-64 bg-custom-beige text-center rounded text-gray-700 font-semibold">
+				<span className="text-2xl">Hello</span>
+				<span className="text-2xl">dsdsd</span>
+				<span className="text-2xl">dsdsd</span>
+				<span className="text-2xl">helloh</span>
+			</div>
+			<div className="w-full">
+				<h1 className="text-3xl py-4">Translator App</h1>
+				<button>Start</button>
+			</div>
+		</main>
+	);
+};
+
+export default Translator;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,22 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		"./pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+			colors: {
+				background: "var(--background)",
+				foreground: "var(--foreground)",
+			},
+			backgroundImage: {
+				"custom-beige":
+					"linear-gradient(44.8deg, rgba(255, 136, 102, 0.67) -53.1%, rgba(255, 221, 136, 0.28) 49%)",
+			},
+		},
+	},
+	plugins: [],
 } satisfies Config;

21025c7c8060346c15bca0ec3fef829b473fa77a
fawzisilver <90504301+fawzisilver@users.noreply.github.com>
2024-11-08 21:57:13 -0700

    added states (needs more work)

--- app/translator/page.tsx
+++ app/translator/page.tsx
@@ -6,6 +6,7 @@ import { IoReload } from "react-icons/io5";
 import { CiCircleChevDown } from "react-icons/ci";
 import Link from "next/link";
 import { languages } from "@/dataset/languagesData.js";
+import { TextArea } from "@/component/TextArea";
 
 const Translator = () => {
 	const [selectedLanguageFrom, setSelectedLanguageFrom] = useState<string>("en");
@@ -16,24 +17,51 @@ const Translator = () => {
 	);
 
 	const handleLanguageClick = (type: string) => {
+		console.log("im workin!");
 		setCurrentLangSelected(type);
-		setShowLanguages(true);
+		setShowLanguages(!showLanguages);
 	};
 
 	return (
 		<main className="w-full h-screen flex justify-center items-center m-4">
-			<div className="w-[50%] flex flex-col justify-center items-center p-12 bg-[#f5f5f5] rounded relative">
-				<div className="gap-y-4 items-center px-8pb-6 bg-[#f5f5f5]">
+			<div className="min-w-fit min-h-[569px] flex flex-col justify-center items-center py-12 px-4 bg-[#f5f5f5] rounded relative">
+				<div className="gap-y-4 items-center  bg-[#f5f5f5]">
 					<Link href="/">
 						<button className="absolute top-4 left-4 text-gray-700">
 							<FaRegRectangleXmark />
 						</button>
 					</Link>
-					<div className="w-[28rem] min-h-16 flex gap-10 justify-center items-center mb-4 text-gray-700 bg-custom-beige font-medium rounded-md shadow-md">
-						<div className="cursor-pointer font-semibold">English</div>
+					<div className="w-[28rem] min-h-16 flex gap-10 justify-center items-center mb-4 text-gray-700 bg-[#cc9767] font-medium rounded-md shadow-md">
+						<div
+							onClick={() => handleLanguageClick("from")}
+							className="cursor-pointer font-semibold"
+						>
+							English
+						</div>
 						<IoReload />
-						<div className="cursor-pointer font-semibold">English</div>
+						<div
+							onClick={() => handleLanguageClick("to")}
+							className="cursor-pointer font-semibold"
+						>
+							English
+						</div>
 					</div>
+
+					{showLanguages && (
+						<div className="w-[93.5%] h-[calc(100%-8rem)] bg-[#fff0db] absolute top-[7rem] z-10 rounded shadow-lg overflow-y-auto">
+							<ul>
+								{Object.entries(languages).map(([key, value]) => (
+									<li
+										key={key}
+										className="text-center cursor-pointer hover:bg-slate-800 hover:text-white transition duration-200 px-10 py-[0.1rem] rounded"
+									>
+										{value}
+									</li>
+								))}
+							</ul>
+						</div>
+					)}
+
 					<div className="w-full flex justify-center relative">
 						<textarea className="w-[28rem] h-44 shadow-md rounded-lg px-5 py-2" />
 						<div className="absolute bottom-1 right-11 text-xs">0/200</div>
@@ -43,7 +71,6 @@ const Translator = () => {
 							<CiCircleChevDown style={{ height: "25px", width: "25px" }} />
 						</button>
 					</div>
-
 					<div className="w-full">
 						<textarea className="w-[28rem] flex justify-center h-44 shadow-md rounded-lg px-5 py-2" />
 					</div>
---
+++ component/TextArea.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { CiCircleChevDown } from "react-icons/ci";
+
+export function TextArea() {
+	return <></>;
+}
